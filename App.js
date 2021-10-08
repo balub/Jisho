1b5e37d8ef755b37c291862dd8aed215ea2d592e
Balu Babu <balub997@gmail.com>
2021-10-09 01:27:29 +0530

    Carosel implemented with custom font

--- App.js
+++ App.js
@@ -1,25 +1,43 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
+import {
+  useFonts,
+  MontserratAlternates_800ExtraBold,
+  MontserratAlternates_900Black,
+} from "@expo-google-fonts/montserrat-alternates";
+import AppLoading from "expo-app-loading";
+
 import { ThemeContext } from "./src/utils/ThemeContext";
 import BottomNavBar from "./src/components/BottomNavBar";
 import ThemeSwitcher from "./src/components/ThemeSwitcher";
+import TextDisplay from "./src/components/TextDisplay";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    MontserratAlternates_800ExtraBold,
+    MontserratAlternates_900Black,
+  });
   const [lightMode, setLightMode] = useState(false);
-  return (
-    <ThemeContext.Provider value={{ lightMode, setLightMode }}>
-      <SafeAreaView
-        style={{
-          backgroundColor: lightMode ? "#F7F6F7" : "#06062A",
-          ...styles.container,
-        }}
-      >
-        <ThemeSwitcher style={{ position: "absolute", top: 50, right: 20 }} />
-        <BottomNavBar style={{ position: "absolute", bottom: -20 }} />
-      </SafeAreaView>
-    </ThemeContext.Provider>
-  );
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <ThemeContext.Provider value={{ lightMode, setLightMode }}>
+        <SafeAreaView
+          style={{
+            backgroundColor: lightMode ? "#F7F6F7" : "#06062A",
+            ...styles.container,
+          }}
+        >
+          <ThemeSwitcher style={{ position: "absolute", top: 50, right: 20 }} />
+          <TextDisplay style={{ marginTop: 10 }} />
+          <BottomNavBar style={{ position: "absolute", bottom: -20 }} />
+        </SafeAreaView>
+      </ThemeContext.Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- src/components/TextDisplay.js
+++ src/components/TextDisplay.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
- 
+import React, { useContext, useRef } from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import Carousel, { ParallaxImage } from "react-native-snap-carousel";
+
+import { ThemeContext } from "../utils/ThemeContext";
+import Vocabs from "../utils/N5_vocabs.json";
+import { color } from "styled-system";
+
+const SLIDER_WIDTH = Dimensions.get("window").width;
+
 export default function TextDisplay() {
+  const { lightMode, setLightMode } = useContext(ThemeContext);
+  const carouselRef = useRef(null);
+
+  const MeaningCard = ({ item, index }) => {
+    return (
+      <View style={{ alignItems:'center' }}>
+        <Text
+          style={{
+            ...styles.headingText,
+            color: lightMode ? "#06062A" : "#F7F6F7",
+          }}
+        >
+          {item.kanji}
+        </Text>
+        <View
+          style={{
+            ...styles.meaningContainer,
+            backgroundColor: lightMode ? "#06062A" : "#F7F6F7",
+          }}
+        >
+          <Text
+            style={{
+              ...styles.meaningText,
+              color: lightMode ? "#F7F6F7" : "#06062A",
+            }}
+          >
+            {item.hiragana}
+          </Text>
+          <Text
+            style={{
+              ...styles.meaningText,
+              color: lightMode ? "#F7F6F7" : "#06062A",
+            }}
+          >
+            {item.meaning}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>TextDisplay</Text>
-      <StatusBar style="auto" />
+      <Carousel
+        ref={carouselRef}
+        data={Vocabs}
+        renderItem={MeaningCard}
+        sliderWidth={SLIDER_WIDTH}
+        itemWidth={SLIDER_WIDTH}
+        style={{ justifyContent: "center" }}
+        hasParallaxImages={true}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "70%",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  headingText: {
+    fontSize: 100,
+    fontFamily: "MontserratAlternates_800ExtraBold",
+  },
+  meaningContainer: {
+    height: "70%",
+    width: "90%",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  meaningText: {
+    fontSize: 48,
+    fontFamily: "MontserratAlternates_800ExtraBold",
+    textAlign: "center",
   },
 });
